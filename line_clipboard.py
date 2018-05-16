a57a1cb2b958b0bf1873c27bec8b383278ec9820
Peter Hu <Da-Capo@users.noreply.github.com>
2018-05-16 10:44:18 +0800

    Update line_clipboard.py
    简化逻辑和使用

--- line_clipboard.py
+++ line_clipboard.py
@@ -4,34 +4,37 @@ import keyboard
 cache_lines = []
 i = 0
 
+def load_line():
+  global cache_lines
+  try:
+    print('载入第'+str(i)+'条:',cache_lines[i])
+    clipboard.copy(cache_lines[i])
+  except:
+    print('载入异常')
+
 def get_clipboard():
   global cache_lines
+  global i
+  i = 0
   cache_lines = clipboard.paste().split('\n')
   print('复制内容：',cache_lines)
+  load_line()
 
 def next_line():
   global i
   i = i+1
-  print('第'+str(i)+'条')
+  load_line()
   
 def before_line():
   global i
   i = i-1
-  print('第'+str(i)+'条')
-
-def write_line():
-  global cache_lines
-  global i
-  try:
-    print('粘贴',str(i),cache_lines[i])
-    keyboard.write(cache_lines[i])
-  except:
-    print('粘贴异常')
+  load_line()
 
 
-keyboard.add_hotkey('ctrl+alt+c',lambda:get_clipboard())
-keyboard.add_hotkey('ctrl+alt+z',lambda:next_line())
-keyboard.add_hotkey('ctrl+alt+x',lambda:before_line())
-keyboard.add_hotkey('ctrl+shift+a',lambda:write_line())
+
+keyboard.add_hotkey('ctrl+c',lambda:get_clipboard())
+keyboard.add_hotkey('ctrl+up',lambda:before_line())
+keyboard.add_hotkey('ctrl+down',lambda:next_line())
+keyboard.add_hotkey('ctrl+v',lambda:next_line())
 
 keyboard.wait()
